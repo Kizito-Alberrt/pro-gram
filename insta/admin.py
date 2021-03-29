cf6917e33d6cbf12b3c61b4a551720ca7aa814d6
Kizito-Alberrt <wagabi98@gmail.com>
2021-03-29 19:26:05 +0300

    added classes to admin.py

--- insta/admin.py
+++ insta/admin.py
@@ -1,6 +1,22 @@
 from django.contrib import admin
 from .models import Post
+from django.contrib.auth.admin import UserAdmin as DjangoUserAdmin
+from insta import models
 
 # Register your models here.
 
 admin.site.register(Post)
+
+class UserAdmin(DjangoUserAdmin):
+
+    date_hierachy = 'date_joined'
+    ordering = ['-date_joined']
+    list_display = list(DjangoUserAdmin.list_display) + ['date_joined']
+
+admin.register(models.Post)
+class PostAdmin(admin.ModelAdmin):
+
+    date_hierachy = 'created_at'
+    ordering = ['-created_at']
+    list_display = ['caption', 'user', 'created_at']
+    search_fields = ['caption', 'user__username']
