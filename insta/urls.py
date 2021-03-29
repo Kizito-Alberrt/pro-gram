d890e2ccc81451f0878ad220e7bb7d75ec1f9e29
Kizito-Alberrt <wagabi98@gmail.com>
2021-03-29 19:37:38 +0300

    added url patterns in urls.py

--- insta/urls.py
+++ insta/urls.py
@@ -2,9 +2,26 @@ from django.urls import path, include
 from .views import (
     PostListView
 )
+from . import views
 
 app_name ='insta'
 
 urlpatterns = [
     path('',PostListView.as_view(), name='post_list'),
+    path('', views.PostListView.as_view()),
+	path('posts/', views.PostListView.as_view()),
+	path('posts/create/', views.PostCreateView.as_view()),
+	path('posts/<int:pk>/', views.PostDetailView.as_view()),  # api/v1/instagram/1
+	path('posts/<int:pk>/edit/', views.PostUpdateView.as_view()),
+	
+	# user urls
+	path('users/', views.UserListView.as_view()),
+	path('users/<slug:username>/', views.UserDetailView.as_view()),
+	path('users/<slug:username>/edit/', views.UserUpdateView.as_view()),
+
+	# auth urls
+	path('auth/register/', views.UserCreateView.as_view()),
+	path('auth/login/', views.LoginAPIView.as_view()),
+	path('auth/', include('knox.urls')),
+	path('current_user/', views.current_user),
 ]
